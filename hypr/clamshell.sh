f8d129797c5e01f8abbff75a5b622aaa0e6c24d9
baumertn <github@baumert.me>
2025-07-21 06:44:43 +0200

    fix(hypr): Keep clamshell simpler to work on different pcs

--- hypr/clamshell.sh
+++ hypr/clamshell.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
 LAPTOP_OUTPUT="eDP-1"  # laptop display
-EXTERNAL_OUTPUT="DP-2" # external monitor
 LID_STATE_FILE="/proc/acpi/button/lid/LID/state"
 
 # Read the lid state
 # state: closed
 read -r LS < "$LID_STATE_FILE"
 
-# Strip prefix
-
-# Check if external monitor is connected
-if hyprctl monitors | grep -q "$EXTERNAL_OUTPUT"; then
-    if grep -q "closed" "$LID_STATE_FILE"; then
-        # If lid is closed
-        hyprctl keyword monitor $LAPTOP_OUTPUT,disable
-    else
-        # If lid is open, reload Hyprland to enable eDP-1 and set monitor configurations
-        hyprctl reload
-    fi
+if grep -q "closed" "$LID_STATE_FILE"; then
+    hyprctl keyword monitor $LAPTOP_OUTPUT,disable
 else
     hyprctl reload
 fi
