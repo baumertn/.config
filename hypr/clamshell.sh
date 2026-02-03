ec21c5c79834d08dae78eddf088827b56383e453
baumertn <github@baumert.me>
2026-02-03 15:44:09 +0100

    feat(hypr): Clamshell script now just toggles laptop screen

--- hypr/clamshell.sh
+++ hypr/clamshell.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 LAPTOP_OUTPUT="eDP-1"  # laptop display
-LID_STATE_FILE="/proc/acpi/button/lid/LID/state"
+# LID_STATE_FILE="/proc/acpi/button/lid/LID/state"
+#
+# # Read the lid state
+# # state: closed
+# read -r LS < "$LID_STATE_FILE"
+# if grep -q "closed" "$LID_STATE_FILE"; then
+#     hyprctl keyword monitor $LAPTOP_OUTPUT,disable
+# else
+#     hyprctl reload
+# fi
 
-# Read the lid state
-# state: closed
-read -r LS < "$LID_STATE_FILE"
-
-if grep -q "closed" "$LID_STATE_FILE"; then
+if hyprctl monitors -j | jq -e ".[] | select(.name == \"$LAPTOP_OUTPUT\")"; then
     hyprctl keyword monitor $LAPTOP_OUTPUT,disable
 else
-    hyprctl reload
+    hyprctl keyword monitor $LAPTOP_OUTPUT,preferred,0x0,auto
 fi
