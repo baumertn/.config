34ac27501c1ed7571a855bc45de858a008bf9d33
baumertn <github@baumert.me>
2026-06-01 07:38:16 +0200

    feat(hypr): Clamshell script compatibility with hyprland lua config

--- hypr/clamshell.sh
+++ hypr/clamshell.sh
@@ -13,9 +13,9 @@ LAPTOP_OUTPUT="eDP-1"  # laptop display
 # fi
 
 if hyprctl monitors -j | jq -e ".[] | select(.name == \"$LAPTOP_OUTPUT\")"; then
-    hyprctl dispatch "hl.monitor.({ output = \"$LAPTOP_OUTPUT\", disabled = true })"
+    hyprctl eval "hl.monitor({ output = \"$LAPTOP_OUTPUT\", disabled = true })"
     # hyprctl keyword monitor $LAPTOP_OUTPUT,disable
 else
-    hyprctl dispatch "hl.monitor.({ output = 'eDP-1', mode = 'preffered', position = '0x0', scale = 'auto', })"
+    hyprctl eval "hl.monitor({ output = 'eDP-1', mode = 'preffered', position = '0x0', scale = 'auto', })"
     # hyprctl keyword monitor $LAPTOP_OUTPUT,preferred,0x0,auto
 fi
