45614513c60a3e7b29cf27fbf95e61086df03c70
baumertn <github@baumert.me>
2026-06-01 12:29:23 +0200

    feat(nvim): More clean up

--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -65,29 +65,37 @@
 ---
 
 local root_markers1 = {
-  '.emmyrc.json',
-  '.luarc.json',
-  '.luarc.jsonc',
+	".emmyrc.json",
+	".luarc.json",
+	".luarc.jsonc",
 }
 local root_markers2 = {
-  '.luacheckrc',
-  '.stylua.toml',
-  'stylua.toml',
-  'selene.toml',
-  'selene.yml',
+	".luacheckrc",
+	".stylua.toml",
+	"stylua.toml",
+	"selene.toml",
+	"selene.yml",
 }
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
-  root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers1, root_markers2, { '.git' } }
-    or vim.list_extend(vim.list_extend(root_markers1, root_markers2), { '.git' }),
-  ---@type lspconfig.settings.lua_ls
-  settings = {
-    Lua = {
-      codeLens = { enable = true },
-      hint = { enable = true, semicolon = 'Disable' },
-    },
-  },
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers1, root_markers2, { ".git" } }
+		or vim.list_extend(vim.list_extend(root_markers1, root_markers2), { ".git" }),
+	workspace = {
+		checkThirdParty = false,
+		library = {
+			vim.env.VIMRUNTIME,
+			-- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
+			vim.api.nvim_get_runtime_file("lua/lspconfig", false)[1],
+		},
+	},
+	---@type lspconfig.settings.lua_ls
+	settings = {
+		Lua = {
+			codeLens = { enable = true },
+			hint = { enable = true, semicolon = "Disable" },
+		},
+	},
 }
